5e5d23c11940eaddb66925ed5e26f6f914fc3f28
cutem-0104 <shuhei.sugaya@gmail.com>
2020-03-31 09:41:13 +0000

    add error check

--- discover.py
+++ discover.py
@@ -1,11 +1,14 @@
 import unittest, sys
 
 def suite():
-	test_suite = unittest.TestSuite()
-	all_test_suite = unittest.defaultTestLoader.discover("test", pattern="test_*.py")
-	for ts in all_test_suite:
-		test_suite.addTest(ts)
-	return test_suite
+	try:
+		test_suite = unittest.TestSuite()
+		all_test_suite = unittest.defaultTestLoader.discover("test", pattern="test_*.py")
+		for ts in all_test_suite:
+			test_suite.addTest(ts)
+		return test_suite
+	except unitetest.SkipTest as e:
+		sys.exit(1)
 
 if __name__ == "__main__":
 	mySuite = suite()
