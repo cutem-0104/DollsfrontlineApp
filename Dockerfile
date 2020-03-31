3b0404b003601cc9ed6928a0f03ff8872b61ecb3
cutem-0104 <shuhei.sugaya@gmail.com>
2020-03-31 09:53:35 +0000

    revert files

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM evarga/jenkins-slave
 RUN apt-get update -y && apt-get upgrade -y
 RUN apt-get install -y python3 python3-pip
-
+RUN pip3 install PyYAML
+RUN pip3 install beautifulsoup4
--- discover.py
+++ discover.py
@@ -1,21 +1,13 @@
-import unittest, sys
+import unittest
 
 def suite():
-	try:
-		test_suite = unittest.TestSuite()
-		all_test_suite = unittest.defaultTestLoader.discover("test", pattern="test_*.py")
-		for ts in all_test_suite:
-			test_suite.addTest(ts)
-		return test_suite
-	except Exception as e:
-		print(e)
-		sys.exit(1)
+	test_suite = unittest.TestSuite()
+	all_test_suite = unittest.defaultTestLoader.discover("test", pattern="test_*.py")
+	for ts in all_test_suite:
+		test_suite.addTest(ts)
+	return test_suite
 
 if __name__ == "__main__":
-	try:
-		mySuite = suite()
-		unittest.TextTestRunner().run(mySuite)
-	except Exception as e:
-		print(e)
-		sys.exit(1)
+	mySuite = suite()
+	unittest.TextTestRunner().run(mySuite)
 	
